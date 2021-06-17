d6a9fd5831c15c2cea338ba41725c118eeb74310
unknown <felagossanchez@gmail.com>
2021-06-17 18:56:29 -0400

    add src folder

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+ import React from 'react';
+ import { SafeAreaView } from 'react-native';
+
+ export const App = () => {
+
+   return (
+     <SafeAreaView>
+
+     </SafeAreaView>
+   );
+ };
+
+
