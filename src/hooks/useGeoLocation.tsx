d06dd52918a8924ef1631588a2dad1eb1c68a758
unknown <felagossanchez@gmail.com>
2021-06-17 21:26:51 -0400

    hook get current lcoation

---
+++ src/hooks/useGeoLocation.tsx
@@ -0,0 +1,26 @@
+import Geolocation from '@react-native-community/geolocation';
+import { useEffect, useState } from 'react';
+
+interface IGeoLocation {
+    latitude: number;
+    longitude: number;
+}
+
+export const useGeoLocation = () => {
+    const [location, setLocation] = useState<IGeoLocation>({ latitude: 0, longitude: 0 });
+    const [errorLocation, setErrorLocation] = useState("");
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition(
+            response => {
+                const { latitude, longitude } = response.coords;
+                setLocation({ latitude, longitude });
+            },
+            error => setErrorLocation(error.message),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+
+    }, []);
+
+    return { location, errorLocation, setLocation };
+}
