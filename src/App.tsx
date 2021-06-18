d15ac42c105c7036397948b91c73e44264c189a2
unknown <felagossanchez@gmail.com>
2021-06-17 21:27:09 -0400

    add map

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 
- import React from 'react';
- import { SafeAreaView } from 'react-native';
+import React from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { useGeoLocation } from './hooks/useGeoLocation';
 
- export const App = () => {
+export const App = () => {
+  const { location } = useGeoLocation();
+  const { latitude, longitude } = location;
 
-   return (
-     <SafeAreaView>
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE}
+        region={{
+          latitude, longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421
+        }}
+        zoomControlEnabled
+        zoomTapEnabled
+      />
+    </View>
+  );
+};
 
-     </SafeAreaView>
-   );
- };
-
-
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: '#fff'
+  }
+});
